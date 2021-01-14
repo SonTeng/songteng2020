e36c008133ea323319c0e102c78da755a92a2bb1
SongTeng <songteng0604@outlook.com>
2021-01-14 22:10:47 +0800

    代码上传

---
+++ 黑马Java学习笔记及代码 基础班进阶版已完成/黑马java学习过程中全部代码--本人手写/advanced-code/day10-code/src/com/itheima/demo02/CopyFile/Demo02CopyFile.java
@@ -0,0 +1,53 @@
+package com.itheima.demo02.CopyFile;
+
+import java.io.*;
+
+/*
+    文件复制练习:一读一写
+
+    明确:
+        数据源: C:\Users\杜宪\Downloads\TestFileReadByStreamJava.PNG
+        数据的目的地: D:\TestFileReadByStreamJava.PNG
+    文件复制的步骤:
+        1.创建字节缓冲输入流对象,构造方法中传递字节输入流
+        2.创建字节缓冲输出流对象,构造方法中传递字节输出流
+        3.使用字节缓冲输入流对象中的方法read,读取文件
+        4.使用字节缓冲输出流中的方法write,把读取的数据写入到内部缓冲区中
+        5.释放资源(会先把缓冲区中的数据,刷新到文件中)
+
+    文件的大小:1242 字节
+    一次读写一个字节:147毫秒
+    使用数组缓冲读取多个字节,写入多个字节:9毫秒
+ */
+public class Demo02CopyFile {
+    public static void main(String[] args) throws IOException {
+        long s = System.currentTimeMillis();
+        //1.创建字节缓冲输入流对象,构造方法中传递字节输入流
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream("C:\\Users\\杜宪\\Downloads\\TestFileReadByStreamJava.PNG"));
+        //2.创建字节缓冲输出流对象,构造方法中传递字节输出流
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("d:\\TestFileReadByStreamJava.PNG"));
+        //3.使用字节缓冲输入流对象中的方法read,读取文件
+        //一次读取一个字节写入一个字节的方式
+        //测试结果：复制文件共耗时:147毫秒
+        /*
+        int len = 0;
+        while((len = bis.read())!=-1){
+            bos.write(len);
+        }*/
+
+        //使用数组缓冲读取多个字节,写入多个字节
+        //测试结果：复制文件共耗时:9毫秒
+        byte[] bytes = new byte[1024];
+        int len = 0;
+        while((len = bis.read(bytes))!=-1){
+            bos.write(bytes,0,len);
+        }
+
+
+        bos.close();
+        bis.close();
+
+        long e = System.currentTimeMillis();
+        System.out.println("复制文件共耗时:"+(e-s)+"毫秒");
+    }
+}
